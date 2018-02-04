4a4a397cc302accc0f825a2d4e6269441b0c923c
Elijah Buck <elijah@github.com>
2018-02-03 21:50:29 -0800

    basic tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,60 @@
-// You can import your modules
-// const index = require('../index')
-
 test('that we can run tests', () => {
-  // your real tests go here
   expect(1 + 2 + 3).toBe(6)
 })
 
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
+const {createRobot} = require('probot')
+const app = require('probot-labelboard')
+const payload = { "event": "issues", "payload": require('./fixtures/issue.labeled.json')}
+
+describe('probot-labelboard', () => {
+  let robot
+  let github
+
+  beforeEach(() => {
+    // Here we create a robot instance
+    robot = createRobot()
+    // Here we initialize the app on the robot instance
+    app(robot)
+
+    github = {
+      repos: {
+        getContent: jest.fn().mockReturnValue(Promise.resolve({
+          data: { "content": Buffer('bug:\n  repo:\n    tickets: todo').toString('base64')}
+        }))
+      },
+      projects: {
+        getRepoProjects: jest.fn().mockReturnValue(Promise.resolve({
+          // Whatever the GitHub API should return
+          data: [ {"id": 116, "name": "tickets"}, {"id": 117, "name": "meh"} ] 
+        })),
+        getProjectColumns: jest.fn().mockReturnValue(Promise.resolve({
+          data: [ {"id": 331, "name": "todo"}, {"id": 332, "name": "done"}]
+        })),
+        createProjectCard: jest.fn().mockReturnValue(Promise.resolve({
+
+        }))
+      }
+    }
+    // Passes the mocked out GitHub API into out robot instance
+    robot.auth = () => Promise.resolve(github)
+  })
+
+  describe('fetches from api', () => {
+    it('fetches config', async () => {
+      await robot.receive(payload)
+      expect(github.repos.getContent).toHaveBeenCalled()
+    })
+
+    it('fetches projects', async () => {
+      await robot.receive(payload)
+      expect(github.projects.getRepoProjects).toHaveBeenCalled()
+      expect(github.projects.getProjectColumns).toHaveBeenCalled()
+      expect(github.projects.createProjectCard).toHaveBeenCalled()
+    })
+
+    it('creates card', async () => {
+      await robot.receive(payload)
+      expect(github.projects.createProjectCard).toHaveBeenCalled()
+    })
+  })
+})
